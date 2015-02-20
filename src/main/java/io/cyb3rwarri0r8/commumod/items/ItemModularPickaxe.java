958bb29cfcd5fda2e72a4154da9c7274a68ae316
Noah Kovacs <n.kovacs1@icloud.com>
2015-02-20 09:12:11 -0500

    Updated build.gradle
    - Added automatic versioning system for dev builds

--- src/main/java/io/cyb3rwarri0r8/commumod/items/ItemModularPickaxe.java
+++ src/main/java/io/cyb3rwarri0r8/commumod/items/ItemModularPickaxe.java
@@ -2,21 +2,18 @@ package io.cyb3rwarri0r8.commumod.items;
 
 
 
+
 import io.cyb3rwarri0r8.commumod.lib.Reference;
 import io.cyb3rwarri0r8.commumod.main;
-import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
-import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.CraftingManager;
+import net.minecraft.item.ItemPickaxe;
 
 public class ItemModularPickaxe extends Item {
 
     public ItemModularPickaxe()
     {
         super();
-        setHasSubtypes(true);
+
         setUnlocalizedName("modularPickaxe");
         setTextureName(Reference.MODID + ":" + getUnlocalizedName().substring(5));
         setCreativeTab(main.modTab);
