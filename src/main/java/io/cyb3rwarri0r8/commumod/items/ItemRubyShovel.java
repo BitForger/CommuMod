887290440fd38cf81e87cc2aed8fb6df2610ca38
Noah Kovacs <n.kovacs1@icloud.com>
2015-07-09 12:57:17 -0400

    -Fixed the mod not loading - Working on the ruby shovel

--- src/main/java/io/cyb3rwarri0r8/commumod/items/ItemRubyShovel.java
+++ src/main/java/io/cyb3rwarri0r8/commumod/items/ItemRubyShovel.java
@@ -1,9 +1,6 @@
 package io.cyb3rwarri0r8.commumod.items;
 
 
-import cpw.mods.fml.common.FMLLog;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 import io.cyb3rwarri0r8.commumod.Commumod;
 import io.cyb3rwarri0r8.commumod.lib.Reference;
 import net.minecraft.entity.Entity;
@@ -14,9 +11,6 @@ import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.world.World;
 
-/**
- * Created by noah on 8/22/14.
- */
 public class ItemRubyShovel extends ItemSpade {
     public ItemRubyShovel(ToolMaterial material)
     {
@@ -28,20 +22,15 @@ public class ItemRubyShovel extends ItemSpade {
     }
 
     @Override
-    public void onUpdate(ItemStack itemStack, World world, Entity entity, int par4, boolean par5)
+    public void onUpdate(ItemStack itemStack, World world, Entity entity, int slotId, boolean isEquipped)
     {
-        if(!world.isRemote){
-        super.onUpdate(itemStack,world,entity,par4,par5);
-        {
-            EntityPlayer player = (EntityPlayer) entity;
-            ItemStack equipped = player.getCurrentEquippedItem();
-            if (equipped == itemStack)
-            {
-                player.addPotionEffect(new PotionEffect(Potion.digSpeed.id, 5, 4));
+        super.onUpdate(itemStack,world,entity,slotId,isEquipped);
+        if(!world.isRemote) {
+            if (isEquipped && entity instanceof EntityPlayer) {
+                ((EntityPlayer) entity).addPotionEffect(new PotionEffect(Potion.digSpeed.id, 5, 4));
             }
+        }
 
-        }
-        }
     }
 
     @Override
